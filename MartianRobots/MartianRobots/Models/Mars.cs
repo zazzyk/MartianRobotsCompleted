c6cc9912b305e0484c42e403fb79d0f549f9454d
zazzyk <18zahrakhan12@gmail.com>
2024-12-11 21:00:25 +0000

    feat: classes for the robot and for mars

---
+++ MartianRobots/MartianRobots/Models/Mars.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MartianRobots.Models
+{
+    /// <summary>
+    /// Mars is a rectangular grid.(x,y)
+    /// </summary>
+    public class Mars
+    {
+        /// <summary>
+        /// Gets or sets the max x of the coordinate of Mars, set by the input 
+        /// </summary>
+        public int MaxXCooridnate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max y coordinate of Mars, set by the input
+        /// </summary>
+        public int MaxYCoordiante { get; set; }
+
+        /// <summary>
+        /// Gets or sets a lists of scents, which are coordiantes that robots have been "lost".
+        /// <see cref="Robot.IsLost"/>
+        /// </summary>
+        public HashSet<(int, int)> Scents { get; } = [];
+    }
+}
---
+++ MartianRobots/MartianRobots/Models/Robot.cs
@@ -0,0 +1,30 @@
+﻿namespace MartianRobots.Models
+{
+    /// <summary>
+    /// A robot on <see cref="Mars"/> that gets instructions from Earth.
+    /// </summary>
+    public class Robot
+    {
+        /// <summary>
+        /// Gets or sets the X coordinate on Mars that the robot is on.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y cooridnate on Mars that the robot is on.
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the compass orintation that the Robot is facing.
+        /// e.g N (North), S (South), E (East), W (West)
+        /// </summary>
+        public char Orientation { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the robot is lost. 
+        /// This means that the robot has gone out of bounds of the planet Mars.
+        /// </summary>
+        public bool IsLost { get; set; }
+    }
+}
