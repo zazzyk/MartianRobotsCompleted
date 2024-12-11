63f8b79e9e09734773f89680b851dc93cde2a67e
zazzyk <18zahrakhan12@gmail.com>
2024-12-11 20:35:12 +0000

    feat: write unit test cases for the robot

---
+++ MartianRobots/MartianRobotsTests/RobotTests.cs
@@ -0,0 +1,45 @@
+namespace MartianRobotsTests
+{
+    public class RobotTests
+    {
+        [Fact]
+        public void RobotTurnsLeft_ChangesPositionCorrectly()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void RobotTurnsRight_ChangesPositionCorrectly()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void RobotGoesForward_MovesForwardsCorrectly()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void RobotGoesOutofBounds_IsMarkedLost()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
--- MartianRobots/MartianRobotsTests/obj/Debug/net8.0/MartianRobotsTests.AssemblyInfo.cs
+++ MartianRobots/MartianRobotsTests/obj/Debug/net8.0/MartianRobotsTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MartianRobotsTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+570bb322605a3f317a138f92b3c0ea51ac4b5d26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070a240f78f39f37dd67c09f9451ac835348c73d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MartianRobotsTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MartianRobotsTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
