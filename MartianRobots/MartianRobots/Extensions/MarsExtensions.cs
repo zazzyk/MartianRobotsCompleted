e041503e866fa7a75ab70bf82b32f89855fb7973
zazzyk <18zahrakhan12@gmail.com>
2024-12-11 22:39:29 +0000

    refactor: move logic of mars code into an extension method

---
+++ MartianRobots/MartianRobots/Extensions/MarsExtensions.cs
@@ -0,0 +1,41 @@
+﻿using MartianRobots.Models;
+
+namespace MartianRobots.Extensions
+{
+    public static class MarsExtensions
+    {
+        /// <summary>
+        /// Calculates whether the robot has gone out of the bounds of Mars
+        /// </summary>
+        /// <param name="x">X coordinate to check</param>
+        /// <param name="y">Y coordinate to check</param>
+        /// <returns></returns>
+        public static bool IsOutOfBounds(this Mars mars, int x, int y)
+        {
+            return x < 0 || x > mars.MaxXCooridnate || y < 0 || y > mars.MaxYCoordiante;
+        }
+
+        /// <summary>
+        /// If a robot is lost (has gone out of bounds of Mars),
+        /// this adds the coorindate it was lost to leave as a scent for other robots.
+        /// </summary>
+        /// <param name="x">X coordinate of Mars</param>
+        /// <param name="y">Y cooridinate of Mars</param>
+        public static void LeaveScent(this Mars mars, int x, int y)
+        {
+            mars.Scents.Add((x, y));
+        }
+
+        /// <summary>
+        /// Checks whethere the coorindates has a Scent.
+        /// If the cooridnates have a scent, the robot will not move there.
+        /// </summary>
+        /// <param name="x">X coordinate to check</param>
+        /// <param name="y">Y coordinate to check</param>
+        /// <returns></returns>
+        public static bool HasScent(this Mars mars, int x, int y)
+        {
+            return mars.Scents.Contains((x, y));
+        }
+    }
+}
--- MartianRobots/MartianRobots/Models/Mars.cs
+++ MartianRobots/MartianRobots/Models/Mars.cs
@@ -29,39 +29,5 @@ namespace MartianRobots.Models
             MaxYCoordiante = maxY;
             Scents = new HashSet<(int, int)>();
         }
-
-        /// <summary>
-        /// Calculates whether the robot has gone out of the bounds of Mars
-        /// </summary>
-        /// <param name="x">X coordinate to check</param>
-        /// <param name="y">Y coordinate to check</param>
-        /// <returns></returns>
-        public bool IsOutOfBounds(int x, int y)
-        {
-            return x < 0 || x > MaxXCooridnate || y < 0 || y > MaxYCoordiante;
-        }
-
-        /// <summary>
-        /// If a robot is lost (has gone out of bounds of Mars),
-        /// this adds the coorindate it was lost to leave as a scent for other robots.
-        /// </summary>
-        /// <param name="x">X coordinate of Mars</param>
-        /// <param name="y">Y cooridinate of Mars</param>
-        public void LeaveScent(int x, int y)
-        {
-            Scents.Add((x, y));
-        }
-
-        /// <summary>
-        /// Checks whethere the coorindates has a Scent.
-        /// If the cooridnates have a scent, the robot will not move there.
-        /// </summary>
-        /// <param name="x">X coordinate to check</param>
-        /// <param name="y">Y coordinate to check</param>
-        /// <returns></returns>
-        public bool HasScent(int x, int y)
-        {
-            return Scents.Contains((x, y));
-        }
     }
 }
